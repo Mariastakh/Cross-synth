f6b4ff90343b3dce5c1fec7d10537a729551b49a
Mariastakh <32569878+Mariastakh@users.noreply.github.com>
2018-12-25 14:12:33 +0000

    Update MainComponent.cpp
    Option to play original sample

--- Cross_Synth/Source/MainComponent.cpp
+++ Cross_Synth/Source/MainComponent.cpp
@@ -1,13 +1,6 @@
 /*
   ==============================================================================
 
- Cross synthesis of two samples, with a filter applied to each sample. The frequency spectrums
- of the original filtered samples and the cross-synthesised result are plotted.
-
-  ==============================================================================
-/*
-  ==============================================================================
-
     This file was auto-generated!
 
   ==============================================================================
@@ -35,14 +28,28 @@ MainComponent::MainComponent()
 	// with the phases of another, will bear different results vice versa.
 	addAndMakeVisible(sampleSwitch);
 	sampleSwitch.setButtonText("Switch Samples");
-	sampleSwitch.onClick = [this] { option = !option; };   // [8]
+	sampleSwitch.onClick = [this] { option = !option;
+	playSample1 = false; playSample2 = false; };   // [8]
 	sampleSwitch.setColour(TextButton::buttonColourId, Colours::skyblue);
+
+	addAndMakeVisible(sample1);
+	sample1.setButtonText("Play Sample 1");
+	sample1.onClick = [this] { playSample1 = true;
+	 playSample2 = false; };   // [8]
+	sample1.setColour(TextButton::buttonColourId, Colours::skyblue);
+
+	addAndMakeVisible(sample2);
+	sample2.setButtonText("Play Sample 2");
+	sample2.onClick = [this] { playSample2 = true; 
+	playSample1 = false; };   // [8]
+	sample2.setColour(TextButton::buttonColourId, Colours::skyblue);
 
 	// Filter Sliders
 	cutoff1.setSliderStyle(Slider::Rotary);
 	cutoff1.setRange(20, 8000);
 	cutoff1.setTextBoxStyle(Slider::TextBoxBelow, false, 100, 20);
 	cutoff1.setColour(Slider::textBoxTextColourId, Colours::black);
+	cutoff1.setColour(Slider::textBoxOutlineColourId, Colours::transparentWhite);
 	cutoff1.onValueChange = [this] { cutoff1Hz = cutoff1.getValue(); };
 	cutoff1.setValue(cutoff1Hz);
 	cutoff1.setLookAndFeel(&otherLookAndFeel);
@@ -56,6 +63,7 @@ MainComponent::MainComponent()
 	res1.setRange(0.1, 2.5);
 	res1.setTextBoxStyle(Slider::TextBoxBelow, false, 100, 20);
 	res1.setColour(Slider::textBoxTextColourId, Colours::black);
+	res1.setColour(Slider::textBoxOutlineColourId, Colours::transparentWhite);
 	res1.onValueChange = [this] { resonance1 = res1.getValue(); };
 	res1.setValue(resonance1);
 	res1.setLookAndFeel(&otherLookAndFeel);
@@ -71,6 +79,7 @@ MainComponent::MainComponent()
 	cutoff2.setRange(20, 2000);
 	cutoff2.setTextBoxStyle(Slider::TextBoxBelow, false, 100, 20);
 	cutoff2.setColour(Slider::textBoxTextColourId, Colours::black);
+	cutoff2.setColour(Slider::textBoxOutlineColourId, Colours::transparentWhite);
 	cutoff2.onValueChange = [this] { cutoff2Hz = cutoff2.getValue(); };
 	cutoff2.setValue(cutoff2Hz);
 	cutoff2.setLookAndFeel(&otherLookAndFeel);
@@ -84,6 +93,7 @@ MainComponent::MainComponent()
 	res2.setRange(0.1, 2.5);
 	res2.setTextBoxStyle(Slider::TextBoxBelow, false, 100, 20);
 	res2.setColour(Slider::textBoxTextColourId, Colours::black);
+	res2.setColour(Slider::textBoxOutlineColourId, Colours::transparentWhite);
 	res2.onValueChange = [this] { resonance2 = res2.getValue(); };
 	res2.setValue(resonance2);
 	res2.setLookAndFeel(&otherLookAndFeel);
@@ -98,6 +108,7 @@ MainComponent::MainComponent()
 	speed1.setRange(0, 10);
 	speed1.setTextBoxStyle(Slider::TextBoxBelow, false, 100, 20);
 	speed1.setColour(Slider::textBoxTextColourId, Colours::black);
+	speed1.setColour(Slider::textBoxOutlineColourId, Colours::transparentWhite);
 	speed1.onValueChange = [this] { sampleSpeed1 = speed1.getValue(); };
 	speed1.setValue(sampleSpeed1);
 	speed1.setLookAndFeel(&otherLookAndFeel);
@@ -112,6 +123,7 @@ MainComponent::MainComponent()
 	speed2.setRange(0, 10);
 	speed2.setTextBoxStyle(Slider::TextBoxBelow, false, 100, 20);
 	speed2.setColour(Slider::textBoxTextColourId, Colours::black);
+	speed2.setColour(Slider::textBoxOutlineColourId, Colours::transparentWhite);
 	speed2.onValueChange = [this] { sampleSpeed2 = speed2.getValue(); };
 	speed2.setValue(sampleSpeed2);
 	speed2.setLookAndFeel(&otherLookAndFeel);
@@ -126,6 +138,7 @@ MainComponent::MainComponent()
 	gain1.setRange(0, 1);
 	gain1.setTextBoxStyle(Slider::TextBoxBelow, false, 100, 20);
 	gain1.setColour(Slider::textBoxTextColourId, Colours::black);
+	gain1.setColour(Slider::textBoxOutlineColourId, Colours::transparentWhite);
 	gain1.onValueChange = [this] { gain_1 = gain1.getValue(); };
 	gain1.setValue(gain_1);
 	gain1.setLookAndFeel(&otherLookAndFeel);
@@ -139,6 +152,7 @@ MainComponent::MainComponent()
 	gain2.setRange(0, 1);
 	gain2.setTextBoxStyle(Slider::TextBoxBelow, false, 100, 20);
 	gain2.setColour(Slider::textBoxTextColourId, Colours::black);
+	gain2.setColour(Slider::textBoxOutlineColourId, Colours::transparentWhite);
 	gain2.onValueChange = [this] { gain_2 = gain2.getValue(); };
 	gain2.setValue(gain_2);
 	gain2.setLookAndFeel(&otherLookAndFeel);
@@ -152,7 +166,7 @@ MainComponent::MainComponent()
 	// with the latest batch of frequency spectrum results so we can represent them:
 	startTimerHz(30);
 
-	setSize(1050, 600);
+	setSize(1050, 580);
 
     // specify the number of input and output channels that we want to open
     setAudioChannels (2, 2);
@@ -193,30 +207,43 @@ void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFil
 		double filteredChords = f1.setCutoff(cutoff1Hz).setResonance(resonance1).play(chords, 1.0, 0.0, 0.0, 0.0);
 		// Apply highpass filter to the second filter:
 		double filteredPerc = f2.setCutoff(cutoff2Hz).setResonance(resonance2).play(perc, 0.0, 0.0, 1.0, 0.0);
-		// Feed samples into the FFT function: This will convert a block of collected amplitude values into
-		// phase and magnitude values (complex values).
-		fft_1.process(filteredChords);
-		fft_2.process(filteredPerc);
-
-		double out = 0;
-
-		// Depending on the toggle button, either combine phases of the first sample with the magnitudes of the second sample,
-		// or combine the phases of the second sample with the magnitudes of the first sample.
-		if (option == true)
+		if (playSample1 == true)
 		{
-			out = ifft.process(fft_2.magnitudes, fft_1.phases);
+			leftBuffer[sample] = filteredChords;
+			rightBuffer[sample] = leftBuffer[sample];
 		}
-		else if (option == false)
+		else if (playSample2 == true)
 		{
-			out = ifft.process(fft_1.magnitudes, fft_2.phases);
+			leftBuffer[sample] = filteredPerc;
+			rightBuffer[sample] = leftBuffer[sample];
 		}
+		else
+		{
+			// Feed samples into the FFT function: This will convert a block of collected amplitude values into
+			// phase and magnitude values (complex values).
+			fft_1.process(filteredChords);
+			fft_2.process(filteredPerc);
 
-		// output
-		leftBuffer[sample] = out * 0.9;
-		rightBuffer[sample] = leftBuffer[sample];
-		// Pass output to another FFT so we can see the frequency spectrum of the cross-synthesis:
-		fft_3.process(out);
-	}
+			double out = 0;
+
+			// Depending on the toggle button, either combine phases of the first sample with the magnitudes of the second sample,
+			// or combine the phases of the second sample with the magnitudes of the first sample.
+			if (option == true)
+			{
+				out = ifft.process(fft_2.magnitudes, fft_1.phases);
+			}
+			else if (option == false)
+			{
+				out = ifft.process(fft_1.magnitudes, fft_2.phases);
+			}
+
+			// output
+			leftBuffer[sample] = out * 0.9;
+			rightBuffer[sample] = leftBuffer[sample];
+			// Pass output to another FFT so we can see the frequency spectrum of the cross-synthesis:
+			fft_3.process(out);
+			}
+		}
 	// Pass the info to the graphics by triggering the callback timer:
 	nextFFTblockReady1 = true;
 }
@@ -244,6 +271,8 @@ void MainComponent::timerCallback()
 void MainComponent::paint (Graphics& g)
 {
 	g.fillAll(Colours::lightgrey);
+	g.setColour(Colours::darkslategrey);
+	g.fillRect(490, 10, 530, 550);
 	drawFrame(g);
     // You can add your drawing code here!
 }
@@ -252,23 +281,28 @@ void MainComponent::paint (Graphics& g)
 void MainComponent::drawFrame(Graphics& g)
 {
 	float xinc = 100 / 1024 * 2.0;
+	if (playSample1 == true || playSample2 == true)
+	{
+		// do nothing
+	}
+	else {
+		// should be until less than 1024/2 (size of fft up to Nyquist)
+		// but we'll only do the first 128 bins so we can fit the
+		// graph into the screen space (most of the activity is happening
+		// at the bottom):
+		for (int i = 0; i < 1024 / 8; i++) {
+			float height1 = fft_1.magnitudes[i] * 5;
+			float height2 = fft_2.magnitudes[i] * 2.5;
+			float height3 = fft_3.magnitudes[i] * 5;
 
-	// should be until less than 1024/2 (size of fft up to Nyquist)
-	// but we'll only do the first 128 bins so we can fit the
-	// graph into the screen space (most of the activity is happening
-	// at the bottom):
-	for (int i = 0; i < 1024 / 8; i++) {
-		float height1 = fft_1.magnitudes[i] * 5;
-		float height2 = fft_2.magnitudes[i] * 5;
-		float height3 = fft_3.magnitudes[i] * 5;
-
-		// Draw a rectangle for every frequency value:
-		g.setColour(Colours::skyblue);
-		g.fillRect(500.2 + (i * 4), 150.1 - height1, 4.0, height1);
-		g.setColour(Colours::grey);
-		g.fillRect(500.2 + (i * 4), 350.1 - height2, 4.0, height2);
-		g.setColour(Colours::orange);
-		g.fillRect(500.2 + (i * 4), 550.1 - height3, 4.0, height3);
+			// Draw a rectangle for every frequency value:
+			g.setColour(Colours::skyblue);
+			g.fillRect(500.2 + (i * 4), 150.1 - height1, 4.0, height1);
+			g.setColour(Colours::grey);
+			g.fillRect(500.2 + (i * 4), 350.1 - height2, 4.0, height2);
+			g.setColour(Colours::orange);
+			g.fillRect(500.2 + (i * 4), 550.1 - height3, 4.0, height3);
+		}
 	}
 }
 
@@ -279,29 +313,31 @@ void MainComponent::resized()
 	cutoff1.setBounds(10, 10, 150, 150);
 	res1.setBounds(10, 200, 150, 150);
 	// Labels:
-	cutoffLabel1.setBounds(50, 160, 50, 50);
-	resLabel1.setBounds(40, 350, 100, 50);
+	cutoffLabel1.setBounds(60, 140, 100, 50);
+	resLabel1.setBounds(40, 325, 100, 50);
 
 	// Sliders for second sample:
 	cutoff2.setBounds(150, 10, 150, 150);
 	res2.setBounds(150, 200, 150, 150);
 	// Labels:
-	cutoffLabel2.setBounds(190, 160, 100, 50);
-	resLabel2.setBounds(190, 350, 100, 50);
+	cutoffLabel2.setBounds(200, 140, 100, 50);
+	resLabel2.setBounds(190, 325, 100, 50);
 
 	// Speed Slider 1:
 	speed1.setBounds(10, 390, 150, 150);
 	// Label
-	speedLabel1.setBounds(40, 540, 100, 50);
+	speedLabel1.setBounds(55, 520, 100, 50);
 	// Speed Slider 2:
 	speed2.setBounds(150, 390, 150, 150);
 	// Label:
-	speedLabel2.setBounds(170, 540, 100, 50);
+	speedLabel2.setBounds(195, 520, 100, 50);
 
-	// Button:
+	// Buttons:
 	sampleSwitch.setBounds(340, 260, 100, 70);
+	sample1.setBounds(355, 30, 70, 35);
+	sample2.setBounds(355, 80, 70, 35);
 
 	// Gain sliders:
 	gain1.setBounds(340, 140, 100, 100);
-	gain2.setBounds(340, 340, 100, 100);
+	gain2.setBounds(340, 355, 100, 100);
 }
